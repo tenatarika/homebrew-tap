a7719891c0aa0523417621a31fb44d1425ca5dda
Furcas <tenatarika@gmail.com>
2026-05-07 19:16:54 +0200

    bump vex to v0.2.0

--- Formula/vex.rb
+++ Formula/vex.rb
@@ -1,8 +1,8 @@
 class Vex < Formula
   desc "Fast hybrid structural + semantic code search (vector + index)"
   homepage "https://github.com/tenatarika/vex"
-  url "https://github.com/tenatarika/vex/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b7dec58dd0c372c9632d2d420296561f709514e9a4f101d7fd1553aaa116e5a8"
+  url "https://github.com/tenatarika/vex/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "81b1c0393d70eba94997e458305e09de5b6ddfecdebacf7413514fd981015922"
   license "MIT"
   head "https://github.com/tenatarika/vex.git", branch: "main"
 
